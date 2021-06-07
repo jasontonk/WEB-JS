8166556e737ed1bfa4cf8d9d787fdb58a61bb4f6
Vincent <63718020+Vincent2162102@users.noreply.github.com>
2021-06-07 11:44:11 +0100

    navbar to switch page added

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -13,6 +13,35 @@ export default class MainController {
 
         let grid = new GridController();
         let drag = new DragAndDropController(grid);
+
+        this.app = document.getElementById("app");
+        this.checkNav();
     }
 
+    SwitchToView(e) {
+        switch (e.target.id){
+            case "sim":
+                this.app.innerHTML = '';
+                this.renderSim();
+                break;
+            case "home":
+                this.app.innerHTML = '';
+                this.renderHome();
+                break;
+        }
+
+    }
+
+    checkNav() {
+        document.getElementById("sim").addEventListener('click', evt => this.SwitchToView(evt), false);
+        document.getElementById("home").addEventListener('click', evt => this.SwitchToView(evt), false);
+    }
+
+    renderHome() {
+        this.app.innerHTML = "this is the homepage";
+    }
+
+    renderSim() {
+        this.app.innerHTML = "this is the Simulation";
+    }
 }
